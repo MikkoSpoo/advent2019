cce1c1b5a0ae6e93866d57d58acdc14d0c61faba
Mikko Saarinen <mikko.saarinen@iki.fi>
2019-12-09 20:48:48 +0200

    Intcode: outputs as vector

--- src/bin/intcode/mod.rs
+++ src/bin/intcode/mod.rs
@@ -3,15 +3,15 @@
 // https://adventofcode.com/2019/day/2
 // https://adventofcode.com/2019/day/5
 
-pub fn run(ram: &mut Vec<i32>, input: i32) -> Option<i32>{
+pub fn run(ram: &mut Vec<i32>, input: i32) -> Vec<i32> {
     //println!("Hello, world! ram={:?}", ram);
     let mut pc: usize = 0;
-    let mut last_output: Option<i32> = None;
+    let mut v_output: Vec<i32> = vec![];
     loop {
         let (optnewpc, optoutput) = step(pc, ram, input);
         match optoutput {
             None => {},
-            Some(_) => {last_output = optoutput }
+            Some(output) => { v_output.push(output) }
         }
         match optnewpc {
             Some(newpc) => pc = newpc,
@@ -19,7 +19,7 @@ pub fn run(ram: &mut Vec<i32>, input: i32) -> Option<i32>{
         }
     }
     //println!("Goodbye, world! ram={:?} Last output={:?}", ram, last_output);
-    return last_output;
+    return v_output;
 }
 
 fn getarglocs(pc: usize, numofargs: usize, ram: &mut Vec<i32>)
@@ -113,49 +113,54 @@ fn step(pc: usize, ram: &mut Vec<i32>, input: i32)
     };
 }
 
+// for test
+fn rtl(ram: &mut Vec<i32>, input: i32) -> Option<i32> {
+    Some(*(run(ram, input).last()?))
+}
+
 #[test]
 fn t_intcode_aoc5_phase2() {
-    assert_eq!(run(&mut vec![3,9,8,9,10,9,4,9,99,-1,8], 21),
+    assert_eq!(rtl(&mut vec![3,9,8,9,10,9,4,9,99,-1,8], 21),
                Some(0));
-    assert_eq!(run(&mut vec![3,9,8,9,10,9,4,9,99,-1,8], 8),
+    assert_eq!(rtl(&mut vec![3,9,8,9,10,9,4,9,99,-1,8], 8),
                Some(1));
 
     // Using position mode, consider whether the input is
     // less than 8; output 1 (if it is) or 0 (if it is not).
-    assert_eq!(run(&mut vec![3,9,7,9,10,9,4,9,99,-1,8], 7), Some(1));
-    assert_eq!(run(&mut vec![3,9,7,9,10,9,4,9,99,-1,8], 8), Some(0));
-    assert_eq!(run(&mut vec![3,9,7,9,10,9,4,9,99,-1,8], 9), Some(0));
+    assert_eq!(rtl(&mut vec![3,9,7,9,10,9,4,9,99,-1,8], 7), Some(1));
+    assert_eq!(rtl(&mut vec![3,9,7,9,10,9,4,9,99,-1,8], 8), Some(0));
+    assert_eq!(rtl(&mut vec![3,9,7,9,10,9,4,9,99,-1,8], 9), Some(0));
 
     // Using immediate mode, consider whether the input is
     // less than 8; output 1 (if it is) or 0 (if it is not).
-    assert_eq!(run(&mut vec![3,3,1107,-1,8,3,4,3,99], 7), Some(1));
-    assert_eq!(run(&mut vec![3,3,1107,-1,8,3,4,3,99], 8), Some(0));
-    assert_eq!(run(&mut vec![3,3,1107,-1,8,3,4,3,99], 9), Some(0));
+    assert_eq!(rtl(&mut vec![3,3,1107,-1,8,3,4,3,99], 7), Some(1));
+    assert_eq!(rtl(&mut vec![3,3,1107,-1,8,3,4,3,99], 8), Some(0));
+    assert_eq!(rtl(&mut vec![3,3,1107,-1,8,3,4,3,99], 9), Some(0));
 
     // Using immediate mode, consider whether the input is equal to 8;
     // output 1 (if it is) or 0 (if it is not).
-    assert_eq!(run(&mut vec![3,3,1108,-1,8,3,4,3,99], 7), Some(0));
-    assert_eq!(run(&mut vec![3,3,1108,-1,8,3,4,3,99], 8), Some(1));
-    assert_eq!(run(&mut vec![3,3,1108,-1,8,3,4,3,99], 9), Some(0));
+    assert_eq!(rtl(&mut vec![3,3,1108,-1,8,3,4,3,99], 7), Some(0));
+    assert_eq!(rtl(&mut vec![3,3,1108,-1,8,3,4,3,99], 8), Some(1));
+    assert_eq!(rtl(&mut vec![3,3,1108,-1,8,3,4,3,99], 9), Some(0));
 
     // Here are some jump tests that take an input, then output 0 if
     // the input was zero or 1 if the input was non-zero:
-    assert_eq!(run(&mut vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0), Some(0));
-    assert_eq!(run(&mut vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 888), Some(1));
-    assert_eq!(run(&mut vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0), Some(0));
-    assert_eq!(run(&mut vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 888), Some(1));
+    assert_eq!(rtl(&mut vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0), Some(0));
+    assert_eq!(rtl(&mut vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 888), Some(1));
+    assert_eq!(rtl(&mut vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0), Some(0));
+    assert_eq!(rtl(&mut vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 888), Some(1));
 
 
 
-    assert_eq!(run(&mut vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+    assert_eq!(rtl(&mut vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
                              1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
                              999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 5),
                Some(999));
-    assert_eq!(run(&mut vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+    assert_eq!(rtl(&mut vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
                              1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
                              999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 8),
                Some(1000));
-    assert_eq!(run(&mut vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+    assert_eq!(rtl(&mut vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
                              1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
                              999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 9),
                Some(1001));
